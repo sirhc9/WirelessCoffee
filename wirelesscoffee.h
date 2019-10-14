cadc798f87d9a582c1281f85349931379b844cf7
sirhc9 <37290644+sirhc9@users.noreply.github.com>
2019-10-14 19:30:26 +1000

    Header for working begin and loop as receiver only

--- wirelesscoffee.h
+++ wirelesscoffee.h
@@ -4,14 +4,23 @@
 #ifndef WIRELESSCOFFEE_H_
 #define WIRELESSCOFFEE_H_
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
 class WirelessCoffee {
+  
+  RF24* radio;
+  
   public:
-    begin(); //Initalises read/write communication
-    end();	 //For shutdown? (Needed?)
-    sleep(); //Enter low power mode
-    wake();  //Leave low power mode (Needed?)
-    requestStatus(); //Request coffee status from network
-    sendStatus(); //Broadcast coffee status to network
+    WirelessCoffee();
+    void begin(int CE, int CSN, HardwareSerial* serialDebug = nullptr); //Initalises read/write communication
+    void end();	 //For shutdown? (Needed?)
+    void sleep(); //Enter low power mode
+    void wake();  //Leave low power mode (Needed?)
+    void requestStatus(); //Request coffee status from network
+    void sendStatus(); //Broadcast coffee status to network
+    void loop(); //Perform loop tasks
 };
 
 #endif /* WIRELESSCOFFEE_H_ */
