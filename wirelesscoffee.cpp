8fcf6403036902248f786baff64422d7a7409fbf
sirhc9 <37290644+sirhc9@users.noreply.github.com>
2019-10-14 19:30:04 +1000

    Basic begin and loop code for receiving only

--- wirelesscoffee.cpp
+++ wirelesscoffee.cpp
@@ -1,3 +1,28 @@
-/*
-  Drink said beans
-*/
+#include <wirelesscoffee.h>
+
+WirelessCoffee::WirelessCoffee()
+{
+  
+}
+
+void WirelessCoffee::begin(int CE, int CSN, HardwareSerial* serialDebug = nullptr)
+{
+  const byte address[6] = "00001";
+  radio = new RF24(CE, CSN);
+  radio->begin();
+  radio->openReadingPipe(0, address);
+  radio->setPALevel(RF24_PA_MIN);
+  radio->startListening();
+  if (serialDebug != nullptr)
+    Serial.print("Done!");
+}
+
+void WirelessCoffee::loop()
+{
+  if (radio->available()) {
+    char text[32] = "";
+    radio->read(&text, sizeof(text));
+    //if (text == "Hello World")
+      Serial.println(text);
+  }
+}
