4e2946750c88e3efec227eda215e26246a6926df
sirhc9 <37290644+sirhc9@users.noreply.github.com>
2019-10-14 19:09:06 +1000

    Added placeholder functions

--- wirelesscoffee.h
+++ wirelesscoffee.h
@@ -1,3 +1,17 @@
 /*
-  Roast some beans
+  Todo: Quit wasting time and roast some beans
 */
+#ifndef WIRELESSCOFFEE_H_
+#define WIRELESSCOFFEE_H_
+
+class WirelessCoffee {
+  public:
+    begin(); //Initalises read/write communication
+    end();	 //For shutdown? (Needed?)
+    sleep(); //Enter low power mode
+    wake();  //Leave low power mode (Needed?)
+    requestStatus(); //Request coffee status from network
+    sendStatus(); //Broadcast coffee status to network
+};
+
+#endif /* WIRELESSCOFFEE_H_ */
