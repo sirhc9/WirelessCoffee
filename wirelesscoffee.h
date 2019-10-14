6b8e5b354462d41305a54331688a3f31186fbcc9
sirhc9 <37290644+sirhc9@users.noreply.github.com>
2019-10-14 22:12:41 +1000

    Broken with message types slowly being implemented

--- wirelesscoffee.h
+++ wirelesscoffee.h
@@ -8,19 +8,36 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+
 class WirelessCoffee {
   
   RF24* radio;
   
+  static const byte readAddress[6];
+  static const byte writeAddress[6];
+  
+  struct MSG_TYPE
+  {
+    static const char STATUS_BROADCAST = 1;
+  };
+  
+  enum class _COFFEE_STATUS: char
+  {
+    UNKNOWN = 0
+  } COFFEE_STATUS;
+  
+  byte coffeeStatus;
+  char myID[6];
+  
   public:
     WirelessCoffee();
-    void begin(int CE, int CSN, HardwareSerial* serialDebug = nullptr); //Initalises read/write communication
+    void begin(int CE, int CSN, char ID[6], HardwareSerial* serialDebug = nullptr); //Initalises read/write communication
     void end();	 //For shutdown? (Needed?)
     void sleep(); //Enter low power mode
     void wake();  //Leave low power mode (Needed?)
     void requestStatus(); //Request coffee status from network
-    void sendStatus(); //Broadcast coffee status to network
-    void loop(); //Perform loop tasks
+    void sendStatus(HardwareSerial* serialDebug = nullptr); //Broadcast coffee status to network
+    void loop(HardwareSerial* serialDebug = nullptr); //Perform loop tasks
 };
 
 #endif /* WIRELESSCOFFEE_H_ */
