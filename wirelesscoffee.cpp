c2d5da7096760e6c7a1a1261e33c6a153ff5af57
sirhc9 <37290644+sirhc9@users.noreply.github.com>
2019-10-21 07:17:32 +1000

    Add files via upload

--- wirelesscoffee.cpp
+++ wirelesscoffee.cpp
@@ -1,100 +1,218 @@
 #include <wirelesscoffee.h>
 
-const byte WirelessCoffee::readAddress[6] = "_READ";
-const byte WirelessCoffee::writeAddress[6] = "WRITE";
 
-/*
-PACKET STRUCTURE
- - Note max size is 32 bytes
-
-All packets have this pre-pended:
-  TYPE    BYTE[1] //Packet type, see below
-
-Type 1 packets: (Coffee Status broadcast)
-  SENDER  BYTE[5]
-  STATUS  BYTE[1]
-*/
-
-WirelessCoffee::WirelessCoffee()
+//Max packet size is 31 bytes, 1 byte is stolen for the type header-
+RFManager::RFPacket::RFPacket(int size)
 {
-  coffeeStatus = COFFEE_STATUS::UNKNOWN;
+  if (size > 31) size = 31;
+  if (size < 0) size = 0;//0 size packet used as empty packet.
+  myData = new char[size](0);
+  writePos = 0;
+  readPos = 0;
+	myBufferSize = size;
 }
 
-void WirelessCoffee::begin(int CE, int CSN, char ID[6], HardwareSerial* serialDebug = nullptr)
+RFManager::RFPacket::~RFPacket()
+{
+  delete[] myData;
+}
+
+bool RFManager::RFPacket::writeCharArray(char* src, int srcSize)
+{
+  for(char i=0; i<srcSize; ++i)
+  {
+    if (writePos >= myBufferSize) return false;
+    myData[writePos++] = src[i];
+  }
+  return true;
+}
+
+bool RFManager::RFPacket::writeChar(char _char)
+{
+  if (writePos >= myBufferSize) return false;
+  myData[writePos++] = _char;
+  return true;
+}
+
+bool RFManager::RFPacket::readCharArray(char* src, int srcSize)
+{
+  for(char i=0; i<srcSize; ++i)
+  {
+    if (readPos >= myBufferSize) return false;
+    src[readPos++] = myData[i];
+  }
+  return true;
+}
+char RFManager::RFPacket::readChar()
+{
+  if (readPos >= myBufferSize) return false; //This could cause confusing results....
+  return myData[readPos++];
+}
+
+/*RFManager::RFManager()
+{
+  coffeeStatus = COFFEE_STATUS::UNKNOWN;
+}*/
+
+WirelessCoffee::WirelessCoffee(int CE, int CSN, HardwareSerial* serialDebug = nullptr)
+{
+  myRFManager.begin(CE, CSN, serialDebug);
+  //coffeeStatus = COFFEE_STATUS::IDLE;
+  //lastCoffeeStatus = coffeeStatus;
+}
+
+void WirelessCoffee::sendNeedCoffee(HardwareSerial* serialDebug = nullptr)
+{
+  RFManager::RFPacket outPacket;
+	outPacket.writeChar(MSG_TYPE::NORMAL_MESSAGE);
+  outPacket.writeChar(COFFEE_STATUS::NEEDCOFFEE);
+  myRFManager.SendMessage(outPacket, serialDebug);
+  //coffeeStatus = COFFEE_STATUS::NEEDCOFFEE;
+}
+
+void WirelessCoffee::sendGotCoffee(HardwareSerial* serialDebug = nullptr)
+{
+  RFManager::RFPacket outPacket;
+  outPacket.writeChar(MSG_TYPE::NORMAL_MESSAGE);
+	outPacket.writeChar(COFFEE_STATUS::GOTCOFFEE);
+  myRFManager.SendMessage(outPacket, serialDebug);
+  //coffeeStatus = COFFEE_STATUS::GOTCOFFEE;
+}
+
+char WirelessCoffee::listenForStatus(HardwareSerial* serialDebug = nullptr)
+{
+	/*if (myRFManager.available(serialDebug))
+	{
+		RFManager::RFPacket inPacket = myRFManager.receive();
+		return inPacket.readChar();
+	}
+	else
+		return COFFEE_STATUS::NOSTATUS;*/
+	RFManager::RFPacket* inPacket = myRFManager.tryReceive(serialDebug);
+	if (inPacket == nullptr)
+		return COFFEE_STATUS::NOSTATUS;
+	else
+		return inPacket->readChar();
+}
+
+void RFManager::begin(int CE, int CSN, HardwareSerial* serialDebug = nullptr)
 {
   /*
   Arguments
   CE          - Pin on the Arduino for the CE connector
   CSN         - Pin on the Arduino for the CSN connector
-  ID          - 5 bytes of whatever to identify you + a null terminator
-                  Why 5 bytes? Because human names are often 5 letters long
+  ID          - 1 byte to identify the device (like an IP address) 
   serialDebug - Send output debug to a serial connection
   */
-  myID = ID;
   radio = new RF24(CE, CSN);
   radio->begin();
-  radio->openReadingPipe(0, readAddress);
-  radio->openWritingPipe(writeAddress);
+	radio->enableDynamicPayloads();
+  radio->openWritingPipe(broadcastAddress);//Pipe 0 is the only pipe allowed to write as per NRF24L01 spec
+  radio->openReadingPipe(1, broadcastAddress);
   radio->setPALevel(RF24_PA_MIN);
   radio->startListening();
   if (serialDebug != nullptr)
   {
-    Serial.print("Begin done, listening now.");
+    serialDebug->println("Begin done, listening now.");
   }
 }
 
-void WirelessCoffee::loop(HardwareSerial* serialDebug = nullptr)
+/*RFManager::RFPacket RFManager::receive()
 {
+	if (waitingPacket == nullptr) return RFManager::RFPacket(0); //User called then when there is no packet, reply with an empty packet.
+	RFManager::RFPacket returnPacket = *waitingPacket; //Copy the heap packet to stack packet
+	delete waitingPacket; //Destroy the heap packet
+	waitingPacket = nullptr; //Reset the pointer
+	return returnPacket; //Return the stack packet
+}*/
+
+//bool RFManager::available(HardwareSerial* serialDebug = nullptr)
+RFManager::RFPacket* RFManager::tryReceive(HardwareSerial* serialDebug = nullptr)
+{
+	//if (waitingPacket != nullptr) return true; //The user must collect the current packet before reading more
   if (radio->available())
   {
-    char text[32] = "";
-    radio->read(&text, sizeof(text));
-
-    if (serialDebug != nullptr)
+		/*
+		1 byte is for the RF Manager to determine what the packet is for.
+		
+		If the packet type is a reserved type we won't return that data to the user
+		*/
+    RFManager::RFPacket* inPacket = new RFManager::RFPacket(radio->getDynamicPayloadSize());
+    
+		//radio->read(&messageType, 1);
+		radio->read(inPacket->myData, inPacket->myBufferSize);
+		
+    char messageType = inPacket->readChar();
+		
+		if (serialDebug != nullptr)
+		{
+			serialDebug->print("Received ");
+			serialDebug->print(inPacket->myBufferSize, DEC);
+			serialDebug->print(" bytes; first is: ");
+			serialDebug->print(messageType, DEC);
+			serialDebug->println();
+		}
+    
+    switch(messageType)
     {
-      Serial.print("Received: ");
-      Serial.println(text);
+      /*case MSG_TYPE::ADDRESS_REQUEST:
+      {
+				RFPacket outPacket;
+				outPacket.writeChar()
+        inPacket.readCharArray(readAddress, ADDR_SIZE);
+				inPacket.readCharArray(writeAddress, ADDR_SIZE);
+        return true;
+      }
+			
+			case MSG_TYPE::ADDRESS_RESPONSE:
+      {
+        inPacket.readCharArray(readAddress, ADDR_SIZE);
+				inPacket.readCharArray(writeAddress, ADDR_SIZE);
+        return true;
+      }*/
+			
+			case MSG_TYPE::NORMAL_MESSAGE:
+      {
+				//waitingPacket = inPacket;
+				return inPacket;
+			}
+      
+      default:
+      {//Unknown message type
+        if (serialDebug != nullptr)
+        {
+          serialDebug->print("Received unknown message type: ");
+          serialDebug->println(messageType);
+        }
+				delete inPacket;
+        return nullptr;
+      }
     }
   }
 }
 
-void WirelessCoffee::sendStatus(HardwareSerial* serialDebug = nullptr)
+void RFManager::SendMessage(RFPacket& packet, HardwareSerial* serialDebug = nullptr)
 {
-  const char text[] = "Hello World";
-  
-  int totalMessageSize = sizeof(MSG_TYPE) 
-                            + sizeof(myID) 
-                            + sizeof(coffeeStatus);
-  
-  char* message = new char[totalMessageSize];
-                            
-  //Compose the messsage
-  int mPos = 0;                      
-  message[mPos++] = MSG_TYPE::STATUS_BROADCAST;
-  
-  for(int i=0; i<sizeof(myID); ++i)
-  {
-    message[mPos++] = myID[i];
-  }
-  message[mPos++] = 0; //Null terminator
-  message[mPos++] = coffeeStatus;
-  
-  if (totalMessageSize != mPos)
-  {
-    if (serialDebug != nullptr)
-    {
-      serialDebug->println("Composed message size isn't what was expected!");
-    }
-  }
-  else
-  {
-    radio->stopListening();
-    radio->write(message, mPos);
-    delete message;
-    radio->startListening();
-    if (serialDebug != nullptr)
-    {
-      serialDebug->println("Message sent successfully");
-    }
-  }
+	radio->stopListening();
+	/*char messageType = MSG_TYPE::NORMAL_MESSAGE;
+	char* tbuff = new char[1+packet.writePos];
+	tbuff[0] = messageType;
+	for(int i=0; i<packet.writePos; ++i)
+			tbuff[i+1] = packet.myData[i];
+	radio->write(tbuff, sizeof(tbuff));*/
+	
+	if (serialDebug != nullptr)
+	{
+		serialDebug->print(packet.writePos, DEC);
+		serialDebug->print(" to send: ");
+		for(char i=0; i<packet.writePos; ++i)
+		{
+			serialDebug->print(packet.myData[i],DEC);
+			serialDebug->print(" ");
+		}
+		serialDebug->println();
+	}
+				
+	radio->write(packet.myData, packet.writePos);
+  radio->startListening();
 }
--- wirelesscoffee.h
+++ wirelesscoffee.h
@@ -4,40 +4,90 @@
 #ifndef WIRELESSCOFFEE_H_
 #define WIRELESSCOFFEE_H_
 
+#define ADDR_SIZE 5
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
+const byte broadcastAddress[ADDR_SIZE+1] = "BROAD"; //+1 to allow for the null terminator, this gets ignored later
 
-class WirelessCoffee {
+class RFManager {
+	
+	//struct RFPacket;
+	protected:
+		
+		struct RFPacket
+		{
+			char* myData;
+			char writePos,readPos,myBufferSize;
+			RFPacket(int size = 31);
+			~RFPacket();
+			bool writeCharArray(char* src, int srcSize);
+			bool writeChar(char _char);
+			bool readCharArray(char* dest, int sizeToRead);
+			char readChar();
+			RFPacket& operator = (const RFPacket& otherPacket) 
+			{
+				if (myBufferSize != otherPacket.myBufferSize)
+				{
+					delete[] myData;
+					myBufferSize = otherPacket.myBufferSize;
+					myData = new char[myBufferSize];//Don't worry about 0 initalisation, it's going to be overwritten
+				}	
+				
+				for(int i=0; i<myBufferSize; ++i)
+					myData[i] = otherPacket.myData[i];
+				
+				writePos = otherPacket.writePos;
+				readPos = otherPacket.readPos;
+					
+				return *this;
+			}
+		};
+		
+	private:
   
-  RF24* radio;
+		RF24* radio;
+		RFPacket* waitingPacket;
+		
+  public:
   
-  static const byte readAddress[6];
-  static const byte writeAddress[6];
-  
-  struct MSG_TYPE
-  {
-    static const char STATUS_BROADCAST = 1;
-  };
-  
-  enum class _COFFEE_STATUS: char
-  {
-    UNKNOWN = 0
-  } COFFEE_STATUS;
-  
-  byte coffeeStatus;
-  char myID[6];
+    struct MSG_TYPE
+    {
+			static const char UNUSED = 0;
+			static const char NORMAL_MESSAGE = 1;
+      static const char ADDRESS_REQUEST = 2;
+      static const char ADDRESS_RESPONSE = 3;
+    };
+    
+    
+    
+    //RFManager();
+    void begin(int CE, int CSN, HardwareSerial* serialDebug = nullptr); //Initalises read/write communication
+    void SendMessage(RFPacket& packet, HardwareSerial* serialDebug = nullptr);
+    //bool available(HardwareSerial* serialDebug = nullptr);
+    RFPacket* tryReceive(HardwareSerial* serialDebug = nullptr);
+		//RFPacket receive();
+};
+
+class WirelessCoffee : protected RFManager {
+  //char coffeeStatus, lastCoffeeStatus;
+  RFManager myRFManager;
   
   public:
-    WirelessCoffee();
-    void begin(int CE, int CSN, char ID[6], HardwareSerial* serialDebug = nullptr); //Initalises read/write communication
-    void end();	 //For shutdown? (Needed?)
-    void sleep(); //Enter low power mode
-    void wake();  //Leave low power mode (Needed?)
-    void requestStatus(); //Request coffee status from network
-    void sendStatus(HardwareSerial* serialDebug = nullptr); //Broadcast coffee status to network
-    void loop(HardwareSerial* serialDebug = nullptr); //Perform loop tasks
+  
+	  struct COFFEE_STATUS
+	  {
+			static const char NOSTATUS = 0;
+			static const char GOTCOFFEE = 1;
+			static const char NEEDCOFFEE = 2;
+	  };
+  
+    WirelessCoffee(int CE, int CSN, HardwareSerial* serialDebug = nullptr);
+    void sendNeedCoffee(HardwareSerial* serialDebug = nullptr);
+    void sendGotCoffee(HardwareSerial* serialDebug = nullptr);
+    char listenForStatus(HardwareSerial* serialDebug = nullptr);
 };
 
 #endif /* WIRELESSCOFFEE_H_ */
